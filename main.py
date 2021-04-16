8eeaede65f129ff5f52ec865f8da4875148dbcee
Bobby Dhammawan Tang <75462550+FeistyFo-x@users.noreply.github.com>
2021-04-17 00:14:26 +0800

    Update main.py
    Nambahin F14 - Load Data

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 # ini biar bisa ngetes aja :v
 
+import os
+import argparse
+parser = argparse.ArgumentParser()                          #
+parser.add_argument("Direktori", nargs='?', default='')     #   Bagian awal program
+args = parser.parse_args()                                  #
+
+if args.Direktori != '':                            
+    try: 
+        print("Loading...")
+        os.chdir(args.Direktori)    #Foldernya ada        
+        print('Selamat datang di "Kantong Ajaib!"')
+    except OSError: # Folder gada
+        print("Folder tidak ditemukan!")
+else:   # Gada input nama folder
+    print("Tidak ada nama folder yang diberikan!")
+    print("Usage: python kantongajaib.py <nama_folder>")
+
+
 from login import login
 from register import register
 from help import help
