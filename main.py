23ace0a0d598f555629c1bf7bfba6a47d5e78a69
Bobby Dhammawan Tang <75462550+FeistyFo-x@users.noreply.github.com>
2021-04-17 12:36:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,14 @@
 
 import os
 import argparse
+from load import load_data
 parser = argparse.ArgumentParser()                          #
 parser.add_argument("Direktori", nargs='?', default='')     #   Bagian awal program
 args = parser.parse_args()                                  #
 
 if args.Direktori != '':                            
-    try: 
         print("Loading...")
-        os.chdir(args.Direktori)    #Foldernya ada        
-        print('Selamat datang di "Kantong Ajaib!"')
-    except OSError: # Folder gada
-        print("Folder tidak ditemukan!")
+        load_data(args.Direktori)
 else:   # Gada input nama folder
     print("Tidak ada nama folder yang diberikan!")
     print("Usage: python kantongajaib.py <nama_folder>")
