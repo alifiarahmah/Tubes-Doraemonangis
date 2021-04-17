79409c6be0a3f53cb164ff8006545764c38648b7
Bobby Dhammawan Tang <75462550+FeistyFo-x@users.noreply.github.com>
2021-04-17 12:34:33 +0800

    Create load.py

---
+++ load.py
@@ -0,0 +1,6 @@
+def load_data(dir_path):
+    try:
+        os.chdir(dir_path) # Nge-set Working Directory
+        print('Selamat datang di "Kantong Ajaib!"') 
+    except OSError: # Foldernya gaada
+        print("Folder tidak ditemukan!")
