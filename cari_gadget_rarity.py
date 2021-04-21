ff695ce89395d9fb421318800c6a19b5dce1fd85
sususaiasusubendera <82642736+sususaiasusubendera@users.noreply.github.com>
2021-04-21 14:28:56 +0700

    Create cari_gadget_rarity.py

---
+++ cari_gadget_rarity.py
@@ -0,0 +1,24 @@
+from csv_stuff import readCSV
+
+def cari_gadget_rarity(): # Menampilkan gadget berdasarkan rarity
+  
+    datas_gadget = readCSV("gadget.csv")
+    
+    input_rarity = str(input("Masukkan rarity: "))
+    print("")
+    print("Hasil pencarian:")
+    print("")
+                           
+    count = 0
+    for i in range(len(datas_gadget)):
+        if input_rarity.upper() == datas_gadget[i][4]:
+            print("Nama              : " + str(datas_gadget[i][1]))
+            print("Deskripsi         : " + str(datas_gadget[i][2]))
+            print("Jumlah            : " + str(datas_gadget[i][3]))
+            print("Rarity            : " + str(datas_gadget[i][4]))
+            print("Tahun Ditemukan   : " + str(datas_gadget[i][5]))
+            print("")
+            count += 1
+                           
+    if count == 0: 
+        print("Tidak ada gadget yang ditemukan")
