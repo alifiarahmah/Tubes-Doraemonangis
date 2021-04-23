91faa29c7ba7524256e59386dd1955f9a073c87c
Bobby Dhammawan Tang <75462550+FeistyFo-x@users.noreply.github.com>
2021-04-23 22:44:43 +0800

    Update load.py

--- load.py
+++ load.py
@@ -7,18 +7,19 @@ def copas(file_lama, file_baru):
 	g.close()
 	f.close()
 
+def setupFile():
+	# dibuat file baru untuk dimodif dalam program
+	# file temp untuk menyimpan file ori/lama, dalam kasus perubahan tidak di-save, file temp menggantikan file baru kembali
+	copas("gadget.csv", "gadget_temp.csv")
+	copas("consumable.csv", "consumable_temp.csv")
+	copas("consumable_history", "consumable_history_temp")
+	copas("gadget_return_history.csv", "gadget_return_history_temp.csv")
+	copas("gadget_borrow_history.csv", "gadget_borrow_history_temp.csv")	
+	
 def load(dir_path):
 	try:
-		os.chdir(dir_path) # Nge-set Current Working Directory
-		
-		# dibuat file baru untuk dimodif dalam program
-		# file temp untuk menyimpan file ori/lama, dalam kasus perubahan tidak di-save, file temp menggantikan file baru kembali
-		copas("gadget.csv", "gadget_temp.csv")
-		copas("consumable.csv", "consumable_temp.csv")
-		copas("consumable_history", "consumable_history_temp")
-		copas("gadget_return_history.csv", "gadget_return_history_temp.csv")
-		copas("gadget_borrow_history.csv", "gadget_borrow_history_temp.csv")	
-		
+		os.chdir(dir_path) # Nge-set Current Working Directory (CWD tidak akan berubah lagi sampai program di reboot)
+		setupFile()
 		print('Selamat datang di "Kantong Ajaib!"') 
 		return True
 	except OSError: # Foldernya gaada
