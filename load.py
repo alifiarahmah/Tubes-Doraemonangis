3e12394796ef53a4d36a8d09468536f9fa4dc357
Bobby Dhammawan Tang <75462550+FeistyFo-x@users.noreply.github.com>
2021-04-23 13:02:23 +0800

    Update load.py

--- load.py
+++ load.py
@@ -3,6 +3,13 @@ import os
 def load(dir_path):
 	try:
 		os.chdir(dir_path) # Nge-set Working Directory
+		
+		os.rename("gadget.csv", "gadget_temp.csv")
+		os.rename("consumable.csv", "consumable_temp.csv")
+		os.rename("consumable_history.csv", "consumable_history_temp")
+		os.rename("gadget_return_history.csv", "gadget_return_history_temp.csv")
+		os.rename("gadget_borrow_history.csv", "gadget_borrow_history_temp.csv")
+		
 		print('Selamat datang di "Kantong Ajaib!"') 
 		return True
 	except OSError: # Foldernya gaada
