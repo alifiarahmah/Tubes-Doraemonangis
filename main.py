617468063113cc085940375c3b756630430562f1
Bobby Dhammawan Tang <75462550+FeistyFo-x@users.noreply.github.com>
2021-04-23 22:49:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from login import login
 from register import register
 from help import help
 from save import save
-from save import save_data_class
+from save import nosave
 from ubahjumlah import ubahjumlah
 from hapusitem import hapusitem
 from tambahitem import tambahitem
@@ -21,9 +21,6 @@ if (args.Direktori != ''):		# ada input argumen untuk nama folder (tidak kosong)
 	print("Loading...")
 	time.sleep(3)
 	if (load(args.Direktori)):  # load -> True kalau foldernya ada, -> False kalau gada
-	
-		save_data = save_data_class("","","")
-		save_empty = save_data_class("","","")
 
 		role = ""
 		user_input = input(">>> ")
@@ -35,38 +32,31 @@ if (args.Direktori != ''):		# ada input argumen untuk nama folder (tidak kosong)
 			elif (user_input == "help"):
 				help(role)
 			elif (user_input == "tambahitem"):
-				save_data = tambahitem()
+				tambahitem()
 			elif (user_input == "ubahjumlah"):
-				save_data = ubahjumlah()
+				ubahjumlah()
 			elif (user_input == "hapusitem"):
-				save_data = hapusitem
+				hapusitem()
 			elif (user_input == "save"):
-				if (save_data == save_empty):
-					print("Tidak ada perubahan yang perlu disimpan")
-				else:
-					save(save_data.header, save_data.datas, save_data.csv_file)
-					save_data = save_empty # reset
-					print("Perubahan file telah disimpan")
+				save()
 			else:
 				print("Perintah tidak valid! (Lihat 'help' untuk melihat list perintah)")
 
 			user_input = input(">>> ")
 
 			if (user_input == "exit"):
-				if (save_data == save_empty):
-					print("Perubahan file sudah disimpan, Terima kasih!")
-				else:
+				ans = str(input("Apakah anda mau melakukan penyimpanan file yang sudah diubah? (y/n) "))
+
+				while ((ans != 'y') and (ans != 'Y') and (ans != 'n') and (ans != 'N')):	
+					print("Jawaban tidak valid, jawab dengan y/n ")
 					ans = str(input("Apakah anda mau melakukan penyimpanan file yang sudah diubah? (y/n) "))
-
-					while ((ans != 'y') and (ans != 'Y') and (ans != 'n') and (ans != 'N')):	
-						print("Jawaban tidak valid, jawab dengan y/n ")
-						ans = str(input("Apakah anda mau melakukan penyimpanan file yang sudah diubah? (y/n) "))
-
-					if ((ans == 'y') or (ans == 'Y')):
-						save(save_data.header, save_data.datas, save_data.csv_file)
-						print("Perubahan file telah disimpan, Terima Kasih!")
-					else: # ((ans == 'n') or (ans == 'N')):	  
-						print("Terima kasih!")	 
+					
+				if ((ans == 'y') or (ans == 'Y')):
+					save()
+					print("Perubahan file telah disimpan, Terima Kasih!")
+				else: # ((ans == 'n') or (ans == 'N')):	 
+					nosave()
+					print("Terima kasih!")	 
 				
 else:	# Gada input argumen untuk nama folder ('')
 	print("Tidak ada nama folder yang diberikan!")
