cb315bb333fb5715d7d3fd11d1fb2dd382637387
alifiarahmah <alifiarahmah03@gmail.com>
2021-04-27 14:02:55 +0700

    bugfix isTglValid

--- tgl.py
+++ tgl.py
@@ -17,14 +17,14 @@ def isKabisat(y):
 
 def isTglValid(tgl): # -> boolean
 	# validasi format tanggal sesuai dd/mm/yyyy
-	if len(tgl) == 9:
+	if len(tgl) == 10:
 		if (tgl[2] == '/') & (tgl[5] == '/'):
 			d = int(tgl[0]) * 10 + int(tgl[1])
 			m = int(tgl[3]) * 10 + int(tgl[4])
 			y = int(tgl[6]) * 1000 + int(tgl[7]) * 100 + int(tgl[8]) * 10 + int(tgl[9])
 
 			if (m >= 1) & (m <= 12): # bulan valid (jan-des)
-				if ((m == 2) & isKabisat(y) & (m <= 29)) | ((m == 2) & (m <= 28)): # Khusus Februari
+				if ((m == 2) & isKabisat(y) & (d >= 1) & (d <= 29)) | ((m == 2) & (d >= 1) & (d <= 28)): # Khusus Februari
 					return True
 				elif ((m <= 7) & (m % 2 == 1)) | ((m >= 8) & (m % 2 == 0)): # 31 hari
 					if (d >= 1) & (d <= 31):
@@ -32,6 +32,3 @@ def isTglValid(tgl): # -> boolean
 				else: # 30 hari
 					if (d >= 1) & (d <= 30):
 						return True
-	return False
-
-print(isTglValid("31/04/2002"))
