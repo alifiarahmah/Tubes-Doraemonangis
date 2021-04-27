83cfa6e3b4427927d7713fdd176164273f4a76d1
theodoreeej <69554894+theodoreeej@users.noreply.github.com>
2021-04-27 15:36:37 +0700

    Rapiin + Update login

--- kantongajaib.py
+++ kantongajaib.py
@@ -23,40 +23,42 @@ if (args.Direktori != ''):		# ada input argumen untuk nama folder (tidak kosong)
 	time.sleep(3)
 	if (load(args.Direktori)):  # load -> True kalau foldernya ada, -> False kalau gada
 
-		role = ""
-    user_id = 0
+	role = ""
+	user_id = 0
     
-		user_input = input(">>> ")
+	user_input = input(">>> ")
     
-		while (user_input != "exit"):
-			if (user_input == "login"):
-				tuple_login = login() # nyimpen role dan user id
-        role = tuple_login[0]
-        user_id = tuple_login[1]
-			elif (user_input == "register"):
-				register(role)
-			elif (user_input == "help"):
-				help(role)
+	while (user_input != "exit"):
+		if (user_input == "login"):
+			tuple_login = login(role, user_id) # nyimpen role dan user id
+        		role = tuple_login[0]
+        		user_id = tuple_login[1]
+		elif (user_input == "register"):
+			register(role)
+		elif (user_input == "help"):
+			help(role)
         
-      # Pilihan fungsi dari input
-			elif (user_input == "tambahitem"):
-				tambahitem(role)
-			elif (user_input == "ubahjumlah"):
-				ubahjumlah(role)
-			elif (user_input == "hapusitem"):
-				hapusitem(role)
-			elif (user_input == "save"):
-				save()
-			else:
-				print("Perintah tidak valid! (Lihat 'help' untuk melihat list perintah)")
+      		# Pilihan fungsi dari input
+		elif (user_input == "tambahitem"):
+			tambahitem(role)
+		elif (user_input == "ubahjumlah"):
+			ubahjumlah(role)
+		elif (user_input == "hapusitem"):
+			hapusitem(role)
+		elif (user_input == "save"):
+			save()
+		elif (user_input == "exit"):
+			exit()
+		else:
+			print("Perintah tidak valid! (Lihat 'help' untuk melihat list perintah)")
+			print()
 
-			user_input = input(">>> ")
+		user_input = input(">>> ")
 			
-		# user_input == exit
-		exit() 
 				
 else:	# Gada input argumen untuk nama folder ('')
 	print("Tidak ada nama folder yang diberikan!")
 	print("Usage: python kantongajaib.py <nama_folder>")
+	print()
 
 
