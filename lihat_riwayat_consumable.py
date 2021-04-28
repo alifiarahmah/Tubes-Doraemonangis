c0fb0333c6d260e9dfd0a81e3e24a1d01c81dd87
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2021-04-29 01:40:09 +0700

    Create lihat_riwayat_consumable.py

---
+++ lihat_riwayat_consumable.py
@@ -0,0 +1,26 @@
+from csv_stuffs import readCSV
+from tgl import sort_data_date
+
+def lihat_riwayat_consumable(): # Melihat riwayat pengambilan consumable
+    datas_consumable_history = readCSV("consumable_history.csv")
+
+    sort_data_date(datas_consumable_history[1]) # mengurutkan data terbaru berdasarkan tanggal
+
+    if len(datas_consumable_history) <= 5: # saat data <= 5
+        for i in range(5):
+            print("ID Pengambilan      : " + str(datas_consumable_history[1][i][0]))
+            print("Nama Pengambil      : " + str(datas_consumable_history[1][i][1]))
+            print("Nama Consumable     : " + str(datas_consumable_history[1][i][2]))
+            print("Tanggal Pengambilan : " + str(datas_consumable_history[1][i][3]))
+            print("Jumlah              : " + str(datas_consumable_history[1][i][4]))
+            print("")
+    else: # saat data > 5
+        idx = 0
+        while idx < 5:
+            print("ID Pengambilan      : " + str(datas_consumable_history[1][idx][0]))
+            print("Nama Pengambil      : " + str(datas_consumable_history[1][idx][1]))
+            print("Nama Consumable     : " + str(datas_consumable_history[1][idx][2]))
+            print("Tanggal Pengambilan : " + str(datas_consumable_history[1][idx][3]))
+            print("Jumlah              : " + str(datas_consumable_history[1][idx][4]))
+            print("")
+            idx += 1
