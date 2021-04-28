fc78ba488cdfdf17005def19a5d0c34f827da23b
alifiarahmah <alifiarahmah03@gmail.com>
2021-04-28 15:40:22 +0700

    added kembalikan and pinjam

---
+++ kembalikan.py
@@ -0,0 +1,86 @@
+from csv_stuffs import readCSV, readCSVdata, addCSVdata, editCSVdata, getCol, getRow
+from tgl import isTglValid
+
+def kembalikan(role, user_id):
+	# mengembalikan seluruh barang. khusus user.
+
+	# KAMUS
+	# datas : array of any
+	# gadget_dipinjam : array of array[1..3] of string
+	# id_peminjam_col, id_gadget, nama_gadget, tgl : string
+	# id_peminjaman, no, jml_pinjam, jml_awal, id : integer
+
+	# ALGORITMA
+
+	if role == "User":
+		
+		datas = readCSV("gadget_borrow_history.csv")[1]
+		id_peminjam_col = getCol("gadget_borrow_history.csv", "id_peminjam")
+		
+		# cari data yang idnya segitu
+
+		# memasukkan daftar gadget yang dipinjam dalam array
+		# format -> [id_peminjaman, id_gadget, nama_gadget]
+		gadget_dipinjam = []
+		
+		for i in range(len(datas)):  # iterasi tiap row dalam datas
+			# untuk setiap pinjam dari user
+			isnt_returned = readCSVdata("gadget_borrow_history.csv", i, getCol("gadget_borrow_history.csv", "is_returned")) == "0"
+			if (readCSVdata("gadget_borrow_history.csv", i, id_peminjam_col) == user_id) & isnt_returned:
+				# ambil id peminjaman
+				id_peminjaman = readCSVdata("gadget_borrow_history.csv", i, getCol("gadget_borrow_history.csv", "id"))
+				# ambil namanya
+				id_gadget = readCSVdata("gadget_borrow_history.csv", i, getCol("gadget_borrow_history.csv", "id_gadget"))
+				nama_gadget = readCSVdata("gadget.csv", getRow("gadget.csv", id_gadget), getCol("gadget.csv", "nama"))
+
+				gadget_dipinjam.append([id_peminjaman, id_gadget, nama_gadget])
+
+		# cek barang pinjaman
+		if len(gadget_dipinjam) > 0:
+
+			# print seluruh data
+			for i in range(len(gadget_dipinjam)):
+				print(str(i+1) + ".", gadget_dipinjam[i][2])
+			print()
+
+			no = int(input("Masukkan nomor peminjaman: "))
+			
+			# validasi nomor peminjaman
+			if (no >= 1) & (no <= len(gadget_dipinjam)):
+				no -= 1
+				id_peminjaman = gadget_dipinjam[no][0]
+				id_gadget = gadget_dipinjam[no][1]
+				nama_gadget = gadget_dipinjam[no][2]
+				
+				# ubah kalo mau bonus
+				jml_pinjam = readCSVdata("gadget_borrow_history.csv", id_peminjaman, getCol("gadget_borrow_history.csv", "jumlah"))
+
+				tgl = input("Tanggal pengembalian: ")
+
+				# validasi tanggal
+				if isTglValid(tgl):
+
+					# ubah jumlah di gadget.csv
+					jml_awal = readCSVdata("gadget.csv", getRow("gadget.csv", id_gadget), getCol("gadget.csv", "jumlah"))
+					editCSVdata("gadget.csv", getRow("gadget.csv", id_gadget), getCol("gadget.csv", "jumlah"), jml_awal+jml_pinjam)
+
+					# catat di gadget_return_history.csv
+					if len(readCSV("gadget_return_history.csv")[1]) == 0: # jika ini entry pertama
+						addCSVdata("gadget_return_history.csv", [0, id_peminjaman, tgl])
+					else: # jika ini entry selanjutnya
+						id = int(readCSVdata("gadget_return_history.csv", (len(readCSV("gadget_return_history.csv")[1])) - 1, 0)) + 1
+						addCSVdata("gadget_return_history.csv", [id, id_peminjaman, tgl])
+
+					# edit is_returned di gadget_borrow_history.csv
+					editCSVdata("gadget_borrow_history.csv", id_peminjaman, getCol("gadget_borrow_history.csv", "is_returned"), 1)
+
+					print("Item", nama_gadget, "(x" + str(jml_pinjam) + ") telah dikembalikan.\n")
+
+			else: # 
+				print("Tidak ada barang dengan nomor peminjaman tersebut!\n")
+
+		else: # len(data_dipinjam) 
+			print("Anda belum pernah meminjam gadget apapun.\n")
+
+	else: # role != "User"
+		print("Admin tidak perlu mengembalikan barang.\n")
---
+++ pinjam.py
@@ -0,0 +1,63 @@
+from csv_stuffs import readCSV, readCSVdata, getCol, getRow, editCSVdata, addCSVdata
+from tgl import isTglValid
+
+def pinjam(role, user_id):
+	# meminjam gadget. khusus user
+
+	# KAMUS LOKAL
+	# item_id, tgl, item
+	# id, jml_pinjam, stok : int
+
+	# ALGORITMA
+
+	# validasi role User
+	if role == "User":
+
+		item_id = input("Masukkan ID item: ")
+
+		if getRow("gadget.csv", item_id) != None: # ID item ada di database
+
+			tgl = input("Tanggal peminjaman: ")
+
+			# validasi tanggal
+			if isTglValid(tgl):
+				
+				jml_pinjam = int(input("Jumlah peminjaman: "))
+
+				if jml_pinjam > 0:
+
+					if getRow("gadget.csv", item_id) != None:  # ID item ada di database
+
+						item = readCSVdata("gadget.csv", getRow("gadget.csv", item_id), getCol("gadget.csv", "nama"))
+						stok = int(readCSVdata("gadget.csv", getRow("gadget.csv", item_id), getCol("gadget.csv", "jumlah")))
+
+						# validasi jumlah peminjaman
+
+						if stok >= jml_pinjam:
+							# ubah jumlah di gadget.csv
+							editCSVdata("gadget.csv", getRow("gadget.csv", item_id), getCol("gadget.csv", "jumlah"), str(stok-jml_pinjam))
+							
+							# catat ke gadget_borrow_history
+							id = int(len(readCSV("gadget_borrow_history.csv")[1])+1)
+							addCSVdata("gadget_borrow_history.csv", [id, user_id, item_id, tgl, jml_pinjam, 0])
+
+							print("Item", item, "(x" + str(jml_pinjam) + ")", "berhasil dipinjam!\n")
+
+						elif stok == 0:
+							print("Item", item, "gagal dipinjam karena stok habis.\n")
+						
+						else: # stok < jml_pinjam
+							print("Item", item, "gagal dipinjam karena jumlah peminjaman melebihi stok.\n")
+
+
+				else: # jml_pinjam < 0
+					print("Jumlah peminjaman tidak valid!\n")
+
+			else: # isTglValid(tgl) == False
+				print("Tanggal tidak valid!\n")
+			
+		else: # ID tidak ada di database
+			print("Tidak ada item dengan ID tersebut!\n")
+
+	else: # role == "Admin"
+		print("Admin tidak perlu melakukan peminjaman pada item.\n")
