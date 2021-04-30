7c62d10a21ecb710f03c976a9c19a5d48ba58fb1
Bobby Dhammawan Tang <75462550+FeistyFo-x@users.noreply.github.com>
2021-04-30 16:14:13 +0800

    Update load.py

--- load.py
+++ load.py
@@ -1,7 +1,8 @@
 import os
 
 def copas(file_lama, file_baru):
-	# membuat file baru dengan isi yang sama dengan file lama 
+	# membuat file baru dengan isi yang sama dengan file lama (khusus csv)
+	
 	g = open(file_lama, "r")
 	f = open(file_baru, "w+")
 	f.write(g.read())
@@ -11,12 +12,20 @@ def copas(file_lama, file_baru):
 def setupFile():
 	# Membuat file temp untuk menyimpan file dengan data awal, Program akan bekerja pada file asli
 	# Dalam kasus perubahan tidak di-save, file temp kembali menggantikan file asli (yang mungkin telah dimodif) 
-	copas("user.csv", "user_temp.csv")
-	copas("gadget.csv", "gadget_temp.csv")
-	copas("consumable.csv", "consumable_temp.csv")
-	copas("consumable_history.csv", "consumable_history_temp.csv")
-	copas("gadget_return_history.csv", "gadget_return_history_temp.csv")
-	copas("gadget_borrow_history.csv", "gadget_borrow_history_temp.csv")	
+	temp = []
+	for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
+		# membuat list of semua file dalam direktori
+		temp.extend(filenames)
+		break
+		
+	ListCSV = []
+	for i in range(len(temp)):
+		# membuat list dari file yang bertipe csv
+		if (temp[i].rfind('.csv') == (len(temp[i])-4)):
+			ListCSV.append(temp[i])
+			
+	for i in range(len(ListCSV)):	
+		copas(ListCSV[i], "temp_" + ListCSV[i]) 
 	
 def load(folder_name):
 	if (os.path.exists(folder_name)): # foldernya ada
@@ -28,3 +37,4 @@ def load(folder_name):
 		print("Folder tidak ditemukan!")
 		return False
 
+# https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
