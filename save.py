f2e665860b77f9d683579ef4f3cac4ff45e3f3cf
Bobby Dhammawan Tang <75462550+FeistyFo-x@users.noreply.github.com>
2021-04-30 16:39:01 +0800

    Update save.py

--- save.py
+++ save.py
@@ -1,25 +1,20 @@
 import os
 import time
+from csv_stuffs import getListCSV
 from load import copas
 from load import setupFile
 
 def nosave():
 	# nosave, dipakai ketika tidak dilakukan penyimpanan di CWD pada saat exit
 	# semua file baru dihapus (bisa jadi disave di folder lain sebelumnya)
-	os.remove("user.csv")
-	os.remove("gadget.csv")
-	os.remove("consumable.csv")
-	os.remove("consumable_history.csv")
-	os.remove("gadget_return_history.csv")
-	os.remove("gadget_borrow_history.csv")
+	RemoveList = getListCSV("no_temp")
+	for i in range(len(RemoveList)):	
+		os.remove(RemoveList[i]) 
 	
-	# file lama (_temp) diubah kembali namanya
-	os.rename("user_temp.csv", "user.csv")
-	os.rename("gadget_temp.csv", "gadget.csv")
-	os.rename("consumable_temp.csv", "consumable.csv")
-	os.rename("consumable_history_temp.csv", "consumable_history.csv")
-	os.rename("gadget_return_history_temp.csv", "gadget_return_history.csv")
-	os.rename("gadget_borrow_history_temp.csv", "gadget_borrow_history.csv")
+	# file lama (temp_) diubah kembali namanya
+	RenameList = getListCSV("only_temp")
+	for i in range(len(RenameList)):	
+		os.rename(RenameList, RemoveList) 
 	
 	return None
 
@@ -32,13 +27,10 @@ def save(exit = False):
 			print("Saving...")
 			time.sleep(3) # efek saving...
 			# file lama (_temp) dihapus, file baru menggantikan
-			os.remove("user_temp.csv")
-			os.remove("gadget_temp.csv")
-			os.remove("consumable_temp.csv")
-			os.remove("consumable_history_temp.csv")
-			os.remove("gadget_return_history_temp.csv")
-			os.remove("gadget_borrow_history_temp.csv")
-			
+			RemoveList = getListCSV("only_temp")
+			for i in range(len(RemoveList)):	
+				os.remove(RemoveList[i]) 	
+				
 			if (not(exit)):
 				# Kasus penyimpanan di tengah program (bukan saat exit)
 				# Dilakukan setupFile seperti pada load, karena ada kemungkinan program masih digunakan setelah save
@@ -59,14 +51,12 @@ def save(exit = False):
 				print("Folder belum ada, akan dibuat folder baru")
 				os.mkdir(dir_save) # foldernya dibuat
 				
-			# path file tujuan = dir_save + "\\csv_file"
+			# path file tujuan = dir_save + "\\" + CopasList[i]
 			# copas ke path tujuan
-			copas("user.csv", dir_save + "\\user.csv")
-			copas("gadget.csv", dir_save + "\\gadget.csv")
-			copas("consumable.csv", dir_save + "\\consumable.csv")
-			copas("consumable_history.csv", dir_save + "\\consumable_history.csv")
-			copas("gadget_return_history.csv", dir_save + "\\gadget_return_history.csv")
-			copas("gadget_borrow_history.csv", dir_save + "\\gadget_borrow_history.csv")
+			CopasList = getListCSV("no_temp")
+				for i in range(len(CopasList)):	
+					copas(CopasList[i], dir_save + "\\" + CopasList[i]) 
+			#copas("user.csv", dir_save + "\\user.csv")
 			
 			if (exit):
 				# Dalam kasus ketika exit, file baru (sudah disimpan di folder lain) dihapus dan file lama (_temp) diganti kembali namanya
