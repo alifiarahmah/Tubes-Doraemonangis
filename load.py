a6cd8e1c2d1fcc1eb933936a71550296ea427111
Bobby Dhammawan Tang <75462550+FeistyFo-x@users.noreply.github.com>
2021-04-30 16:19:43 +0800

    Update load.py

--- load.py
+++ load.py
@@ -1,8 +1,8 @@
 import os
+from csv_stuffs import getListCSV
 
 def copas(file_lama, file_baru):
 	# membuat file baru dengan isi yang sama dengan file lama (khusus csv)
-	
 	g = open(file_lama, "r")
 	f = open(file_baru, "w+")
 	f.write(g.read())
@@ -12,19 +12,7 @@ def copas(file_lama, file_baru):
 def setupFile():
 	# Membuat file temp untuk menyimpan file dengan data awal, Program akan bekerja pada file asli
 	# Dalam kasus perubahan tidak di-save, file temp kembali menggantikan file asli (yang mungkin telah dimodif) 
-	temp = []
-	for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
-		# membuat list of semua file dalam direktori
-		temp.extend(filenames)
-		break
-		
-	ListCSV = []
-	for i in range(len(temp)):
-		# membuat list dari file yang bertipe csv
-		if (temp[i].rfind('.csv') == (len(temp[i])-4)):
-			ListCSV.append(temp[i])
-			
-	for i in range(len(ListCSV)):	
+	for i in range(len(getListCSV())):	
 		copas(ListCSV[i], "temp_" + ListCSV[i]) 
 	
 def load(folder_name):
